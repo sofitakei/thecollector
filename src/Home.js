60477a0c72906f224e2df005543d48bd07a8a5c5
sofitakei <sofi.takei@gmail.com>
2024-10-26 16:15:24 -0700

    updated home text

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,86 @@
-import { Stack, Typography } from '@mui/material'
-import { Navigate } from 'react-router-dom'
+import AssignmentIcon from '@mui/icons-material/Assignment'
+import CheckIcon from '@mui/icons-material/Check'
+import FileOpenIcon from '@mui/icons-material/FileOpen'
+import ForwardToInboxIcon from '@mui/icons-material/ForwardToInbox'
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt'
+import SupportAgentIcon from '@mui/icons-material/SupportAgent'
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Typography,
+} from '@mui/material'
+import { AnimatePresence, motion } from 'framer-motion'
+import { forwardRef } from 'react'
+import { Link, Navigate } from 'react-router-dom'
 
 import { useAuth } from './contexts/AuthContext'
 
-const blurbs = [
-  { left: 'Have some blurb here about how great the product is' },
-  { left: 'And another blurb yay' },
+const MotionBox = motion(Box)
+// eslint-disable-next-line react/display-name
+const StepBox = forwardRef((props, ref) => (
+  <AnimatePresence>
+    <MotionBox
+      ref={ref}
+      borderRadius={3}
+      p={3}
+      m={3}
+      flex={{ xs: '100%', sm: '30%' }}
+      {...props}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.51, ease: 'easeOut' }}
+    />
+  </AnimatePresence>
+))
+
+const steps = [
+  {
+    Icon: PersonAddAltIcon,
+    text: 'Create an account and add your Association',
+    sx: {
+      backgroundColor: 'primary.dark',
+      color: 'primary.contrastText',
+    },
+  },
+  {
+    sx: {
+      border: '1px solid',
+      borderColor: 'primary.light',
+      color: 'text.primary',
+    },
+    Icon: ForwardToInboxIcon,
+    text: 'Invite Beneficial Owners to your Association',
+  },
+  {
+    sx: {
+      backgroundColor: 'primary.light',
+      color: 'primary.contrastText',
+    },
+    Icon: AssignmentIcon,
+    text: 'Collect secured Beneficial Owner information in minutes',
+  },
+  {
+    sx: {
+      backgroundColor: 'primary.dark',
+      color: 'primary.contrastText',
+    },
+    Icon: FileOpenIcon,
+    text: "File your Association's report containing automatically consolidated Beneficial Owner information with a click of a button",
+  },
+  {
+    sx: {
+      border: '1px solid',
+      borderColor: 'primary.light',
+      color: 'text.primary',
+    },
+    Icon: SupportAgentIcon,
+    text: 'Keep Associations compliant with filing requirements with ongoing support with reminders',
+  },
 ]
 
 const Home = () => {
@@ -13,30 +88,57 @@ const Home = () => {
   if (session?.user?.id) return <Navigate to='/properties' />
 
   return (
-    <Stack>
-      {blurbs.map(({ left }, index) => (
-        <Stack
-          key={index}
-          my={2}
-          spacing={2}
-          direction={{ xs: 'column', sm: 'row' }}
-          alignItems='left'
-          sx={{ textAlign: 'left', 'div,h4': { flex: 1 } }}>
-          <Typography variant='h4' width='50%'>
-            {left}
-          </Typography>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </div>
-        </Stack>
-      ))}
-    </Stack>
+    <MotionBox
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, ease: 'easeOut' }}>
+      <Typography variant='h1'>
+        File your Association's Beneficial Ownership Information report with
+        confidence.
+      </Typography>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <CheckIcon />
+          </ListItemIcon>
+          <ListItemText primary='Secure and intuitive step-by-step solution for your Association to file its Beneficial Ownership Information report with FinCEN under the Corporate Transparency Act.' />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <CheckIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary='No payment required until your Association is ready to submit a report for
+      filing.'
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <CheckIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary='Created specifically for Associations by attorney CPA auditors that have
+      over 15 years of working directly and exclusively with Associations.'
+          />
+        </ListItem>
+      </List>
+      <Typography gutterBottom variant='h2'>
+        Steps to file
+      </Typography>
+      <Stack sx={{ flexWrap: 'wrap' }} direction={{ xs: 'column', sm: 'row' }}>
+        {steps.map(({ Icon, text, sx }, idx) => (
+          <StepBox key={idx} sx={sx}>
+            <Icon style={{ fontSize: 40 }} />
+            <Typography variant='step'>Step {idx + 1}</Typography>
+            <div>{text}</div>
+          </StepBox>
+        ))}
+      </Stack>
+      <Box sx={{ a: { color: 'text.primary', textDecoration: 'underline' } }}>
+        Looking for a solution for your Management Company?{' '}
+        <Link to='/contact-us'>Contact us</Link> for enterprise solutions.
+      </Box>
+    </MotionBox>
   )
 }
 
